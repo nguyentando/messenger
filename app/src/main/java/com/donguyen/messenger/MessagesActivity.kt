9d8a6802976e8b37d358f3b0df00fddc037c5ee0
Do Nguyen <donguyen.official@gmail.com>
2018-10-23 10:07:10 +0800

    create the project

---
+++ app/src/main/java/com/donguyen/messenger/MessagesActivity.kt
@@ -0,0 +1,12 @@
+package com.donguyen.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MessagesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messages)
+    }
+}
